9471ba715c48d8d77759d6ce42080f0676e8e6aa
Hirotsumi <danjartex@gmail.com>
2016-10-30 20:22:32 -0200

    Add project files.

---
+++ EstadoCidade/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace EstadoCidade.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ EstadoCidade/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EstadoCidade.Startup))]
+namespace EstadoCidade
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
